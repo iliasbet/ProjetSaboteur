ad2c35ed8f63aebe0b19cf15e92f8852e0fcc2fa
Ilias <74203120+iliasbet@users.noreply.github.com>
2021-03-10 09:18:13 +0100

    Initialisation du code v1

---
+++ fonctions.h
@@ -0,0 +1,7 @@
+#ifndef SABOTEUR_FONCTIONS_H
+#define SABOTEUR_FONCTIONS_H
+
+// Structures
+// Prototypes
+
+#endif
--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+
+#define WIDTH
+#define HEIGHT
 
 int main() {
-    printf("Hello, World!\n");
     return 0;
 }
